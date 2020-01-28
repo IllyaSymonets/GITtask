4803481cdd5bbc2bf3458df680b9ada94d4c176a
IllyaSymonets <illya.symonets@gmail.com>
2020-01-28 16:26:55 +0200

    sub, mult, div, sqrt impl has been added
    mult impl has been added
    
    div impl has been added
    
    sqrt impl has been added

--- Anyname.java
+++ Anyname.java
@@ -2,4 +2,20 @@ public class Anyname {
     public int add(int a, int b) {
         return a + b;
     }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    public int mult(int a, int b) {
+        return a * b;
+    }
+
+    public int div(int a, int b) {
+        return a / b;
+    }
+
+    public int sqrt(int a){
+        return Math.sqrt(a);
+    }
 }
